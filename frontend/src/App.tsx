5807a054bdc1237b9d725cf63ec9911aaa28e201
Yap YuLun <yyulun22@gmail.com>
2025-06-30 02:12:33 +0800

    Fixed AvatarEditorPage import and added missing component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ import ProfilePage from './pages/ProfilePage';
 import DashboardPage from './pages/DashboardPage';
 import ForgotPasswordPage from './pages/ForgotPasswordPage';
 import ResetPasswordPage from './pages/ResetPasswordPage';
-import AvatarEditorPage from './pages/AvatarEditorPage_clean';
+import AvatarEditorPage from './pages/AvatarEditorPage';
 
 
 function App() {
